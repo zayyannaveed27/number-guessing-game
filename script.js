81a5efdfaf4c8a79eb318af4d0795fe9a223e9a9
Zayyan Naveed <zayyan.naveed@yale.edu>
2023-07-17 06:37:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (humanGuess, computerGuess, target) => {
+    const humanDifference = Math.abs(target - humanGuess)
+    const computerDifference = Math.abs(target - computerGuess)
+    return humanDifference <= computerDifference;
+}
+
+const updateScore = (winner) => {
+    if (winner === "human") {
+        humanScore ++;
+    } else {
+        computerScore ++;
+    }
+}
+
+const advanceRound = () => {
+    currentRoundNumber += 1;
+}
